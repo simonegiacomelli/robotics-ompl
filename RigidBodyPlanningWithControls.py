efc0c248b2098912f5aadc6bdfa6d866e32b08e5
simonegiacomelli <simone.giacomelli@gmail.com>
2019-05-13 10:44:36 +0200

    changed robot name

--- RigidBodyPlanningWithControls.py
+++ RigidBodyPlanningWithControls.py
@@ -1,10 +1,11 @@
-    #!/usr/bin/env python
+# !/usr/bin/env python
 
-    # Author: Mark Moll
+# Author: Mark Moll
 
 from math import sin, cos
 from functools import partial
 import numpy as np
+
 try:
     from ompl import base as ob
     from ompl import control as oc
@@ -13,6 +14,7 @@ except ImportError:
     # subdirectory of the parent directory called "py-bindings."
     from os.path import abspath, dirname, join
     import sys
+
     sys.path.insert(0, join(dirname(dirname(abspath(__file__))), 'py-bindings'))
     from ompl import base as ob
     from ompl import control as oc
@@ -21,151 +23,154 @@ except ImportError:
 class MyDecomposition(oc.GridDecomposition):
     def __init__(self, length, bounds):
         super(MyDecomposition, self).__init__(length, 2, bounds)
+
     def project(self, s, coord):
         coord[0] = s.getX()
         coord[1] = s.getY()
+
     def sampleFullState(self, sampler, coord, s):
         sampler.sampleUniform(s)
         s.setXY(coord[0], coord[1])
 
+
 def isStateValid(spaceInformation, state):
-        # perform collision checking or check if other constraints are
-        # satisfied
-        return spaceInformation.satisfiesBounds(state)
+    # perform collision checking or check if other constraints are
+    # satisfied
+    return spaceInformation.satisfiesBounds(state)
+
 
 def mktr(x, y):
-        return np.array([[1, 0, x],
-                        [0, 1, y],
-                        [0, 0, 1]])
+    return np.array([[1, 0, x],
+                     [0, 1, y],
+                     [0, 0, 1]])
+
 
 def mkrot(theta):
-        return np.array([[np.cos(theta), -np.sin(theta), 0],
-                        [np.sin(theta),  np.cos(theta), 0],
-                        [0,              0,             1]])
+    return np.array([[np.cos(theta), -np.sin(theta), 0],
+                     [np.sin(theta), np.cos(theta), 0],
+                     [0, 0, 1]])
+
 
 def ddtr(vl, vr, l, dt):
-        """ returns the pose transform for a motion with duration dt of a differential
-        drive robot with wheel speeds vl and vr and wheelbase l """
+    """ returns the pose transform for a motion with duration dt of a differential
+    drive robot with wheel speeds vl and vr and wheelbase l """
 
-        if(np.isclose(vl, vr)):  # we are moving straight, R is at the infinity and we handle this case separately
-            return mktr((vr + vl)/2*dt, 0)  # note we translate along x ()
+    if (np.isclose(vl, vr)):  # we are moving straight, R is at the infinity and we handle this case separately
+        return mktr((vr + vl) / 2 * dt, 0)  # note we translate along x ()
 
-        omega = (vr - vl) / (2 * l)  # angular speed of the robot frame
-        R = l * (vr + vl) / (vr - vl)
+    omega = (vr - vl) / (2 * l)  # angular speed of the robot frame
+    R = l * (vr + vl) / (vr - vl)
 
-        # Make sure you understand this!
-        return mktr(0, R) @ mkrot(omega * dt) @ mktr(0, -R)
+    # Make sure you understand this!
+    return mktr(0, R) @ mkrot(omega * dt) @ mktr(0, -R)
 
 
-def homogeneous(x,y,theta):
+def homogeneous(x, y, theta):
     return np.array([[np.cos(theta), -np.sin(theta), x],
-                     [np.sin(theta),  np.cos(theta), y],
-                     [0,              0,             1]])
+                     [np.sin(theta), np.cos(theta), y],
+                     [0, 0, 1]])
+
 
 def propagate(start, control, duration, state):
-        """ returns the pose transform for a motion with duration dt of a differential
-        drive robot with wheel speeds vl and vr and wheelbase l """
+    """ returns the pose transform for a motion with duration dt of a differential
+    drive robot with wheel speeds vl and vr and wheelbase l """
 
-        vr = control[0]
-        vl = control[1]
-        l = 1
+    vr = control[0]
+    vl = control[1]
+    l = 1
 
-        relative_pose = ddtr(vl, vr, l, duration)
+    relative_pose = ddtr(vl, vr, l, duration)
 
-        
+    curr_pose = homogeneous(start.getX(), start.getY(), start.getYaw())
 
+    pose = curr_pose @ relative_pose
 
-        curr_pose = homogeneous(start.getX(), start.getY(), start.getYaw())
+    x = pose[0, 2]
+    y = pose[1, 2]
+    yaw = np.arctan2(pose[1, 0], pose[0, 0])
 
-        pose = curr_pose @ relative_pose
+    # print("x = ", x)
+    # print("y = ", y)
+    # print("yaw = ", yaw)
 
-        x = pose[0, 2]
-        y = pose[1, 2]
-        yaw = np.arctan2(pose[1,0], pose[0, 0])
+    # print(control)
+    # print("control[0] = ", control[0])
+    # print("control[1] = ", control[1])
 
-        
+    # state.setX(start.getX() + x)
+    # state.setY(start.getY() + y)
+    # state.setYaw(start.getYaw() + yaw)
 
-        # print("x = ", x)
-        # print("y = ", y)
-        # print("yaw = ", yaw)
+    state.setX(x)
+    state.setY(y)
+    state.setYaw(yaw)
 
-        # print(control)
-        # print("control[0] = ", control[0])
-        # print("control[1] = ", control[1])
-
-        # state.setX(start.getX() + x)
-        # state.setY(start.getY() + y)
-        # state.setYaw(start.getYaw() + yaw)
-
-        state.setX(x)
-        state.setY(y)
-        state.setYaw(yaw)
 
 def plan():
-        # construct the state space we are planning in
-        space = ob.SE2StateSpace()
+    # construct the state space we are planning in
+    space = ob.SE2StateSpace()
 
-        # set the bounds for the R^2 part of SE(2)
-        bounds = ob.RealVectorBounds(2)
-        bounds.setLow(-55)
-        bounds.setHigh(55)
-        space.setBounds(bounds)
+    # set the bounds for the R^2 part of SE(2)
+    bounds = ob.RealVectorBounds(2)
+    bounds.setLow(-55)
+    bounds.setHigh(55)
+    space.setBounds(bounds)
 
-        # create a control space
-        cspace = oc.RealVectorControlSpace(space, 2)
+    # create a control space
+    cspace = oc.RealVectorControlSpace(space, 2)
 
-        # set the bounds for the control space
-        cbounds = ob.RealVectorBounds(2)
-        cbounds.setLow(0,-3.0)
-        cbounds.setHigh(0,3.0)
+    # set the bounds for the control space
+    cbounds = ob.RealVectorBounds(2)
+    cbounds.setLow(0, -3.0)
+    cbounds.setHigh(0, 3.0)
 
-        cbounds.setLow(1,0.0)
-        cbounds.setHigh(1,3.0)
-        cspace.setBounds(cbounds)
+    cbounds.setLow(1, 0.0)
+    cbounds.setHigh(1, 3.0)
+    cspace.setBounds(cbounds)
 
-        # define a simple setup class
-        ss = oc.SimpleSetup(cspace)
-        ss.setStateValidityChecker(ob.StateValidityCheckerFn( \
-        partial(isStateValid, ss.getSpaceInformation())))
-        ss.setStatePropagator(oc.StatePropagatorFn(propagate))
+    # define a simple setup class
+    ss = oc.SimpleSetup(cspace)
+    ss.setStateValidityChecker(ob.StateValidityCheckerFn(partial(isStateValid, ss.getSpaceInformation())))
+    ss.setStatePropagator(oc.StatePropagatorFn(propagate))
 
-        # create a start state
-        start = ob.State(space)
-        start().setX(0.0)
-        start().setY(0.0)
-        start().setYaw(0.0)
+    # create a start state
+    start = ob.State(space)
+    start().setX(0.0)
+    start().setY(0.0)
+    start().setYaw(0.0)
 
-        # create a goal state
-        goal = ob.State(space)
-        goal().setX(24.0)
-        goal().setY(6.0)
-        goal().setYaw(0.0)
+    # create a goal state
+    goal = ob.State(space)
+    goal().setX(24.0)
+    goal().setY(6.0)
+    goal().setYaw(0.0)
 
-        # set the start and goal states
-        ss.setStartAndGoalStates(start, goal, 0.05)
+    # set the start and goal states
+    ss.setStartAndGoalStates(start, goal, 0.05)
 
-        # (optionally) set planner
-        si = ss.getSpaceInformation()
-        # planner = oc.PDST(si)
-        planner = oc.RRT(si)
-        # planner = oc.EST(si)
-        #planner = oc.KPIECE1(si) # this is the default
-        # SyclopEST and SyclopRRT require a decomposition to guide the search
-        decomp = MyDecomposition(32, bounds)
-        # planner = oc.SyclopEST(si, decomp)
-        #planner = oc.SyclopRRT(si, decomp)
-        ss.setPlanner(planner)
-        # (optionally) set propagation step size
-        si.setPropagationStepSize(.1)
+    # (optionally) set planner
+    si = ss.getSpaceInformation()
+    # planner = oc.PDST(si)
+    planner = oc.RRT(si)
+    # planner = oc.EST(si)
+    # planner = oc.KPIECE1(si) # this is the default
+    # SyclopEST and SyclopRRT require a decomposition to guide the search
+    decomp = MyDecomposition(32, bounds)
+    # planner = oc.SyclopEST(si, decomp)
+    # planner = oc.SyclopRRT(si, decomp)
+    ss.setPlanner(planner)
+    # (optionally) set propagation step size
+    si.setPropagationStepSize(.1)
 
-        # attempt to solve the problem
-        solved = ss.solve(40.0)
+    # attempt to solve the problem
+    solved = ss.solve(40.0)
 
-        if solved:
-            # print the path to screen
-            print("Found solution:\n%s" % ss.getSolutionPath().printAsMatrix())
-            with open('path.txt', 'w') as outFile:
-                outFile.write(ss.getSolutionPath().printAsMatrix())
+    if solved:
+        # print the path to screen
+        print("Found solution:\n%s" % ss.getSolutionPath().printAsMatrix())
+        with open('path.txt', 'w') as outFile:
+            outFile.write(ss.getSolutionPath().printAsMatrix())
+
 
 if __name__ == "__main__":
     plan()
---
+++ ompl_app_hack/src/omplapp/apps/KinematicCarPlanning.cpp
@@ -0,0 +1,88 @@
+/*********************************************************************
+* Rice University Software Distribution License
+*
+* Copyright (c) 2010, Rice University
+* All Rights Reserved.
+*
+* For a full description see the file named LICENSE.
+*
+*********************************************************************/
+
+/* Author: Mark Moll */
+
+#include "omplapp/apps/KinematicCarPlanning.h"
+#include <boost/math/constants/constants.hpp>
+
+ompl::app::KinematicCarPlanning::KinematicCarPlanning()
+    : AppBase<AppType::CONTROL>(constructControlSpace(), Motion_2D), odeSolver(std::make_shared<control::ODEBasicSolver<>>(si_, [this](const control::ODESolver::StateType& q, const control::Control *ctrl, control::ODESolver::StateType& qdot)
+    {
+        ode(q, ctrl, qdot);
+    }))
+{
+    name_ = std::string("USI Custom model");
+    setDefaultControlBounds();
+
+    si_->setStatePropagator(control::ODESolver::getStatePropagator(odeSolver,
+        [this](const base::State* state, const control::Control* control, const double duration, base::State* result)
+        {
+            postPropagate(state, control, duration, result);
+        }));
+}
+
+ompl::app::KinematicCarPlanning::KinematicCarPlanning(const control::ControlSpacePtr &controlSpace)
+    : AppBase<AppType::CONTROL>(controlSpace, Motion_2D), timeStep_(1e-2), lengthInv_(1.), odeSolver(std::make_shared<control::ODEBasicSolver<>>(si_, [this](const control::ODESolver::StateType& q, const control::Control *ctrl, control::ODESolver::StateType& qdot)
+    {
+        ode(q, ctrl, qdot);
+    }))
+{
+    setDefaultControlBounds();
+
+    si_->setStatePropagator(control::ODESolver::getStatePropagator(odeSolver,
+        [this](const base::State* state, const control::Control* control, const double duration, base::State* result)
+        {
+            postPropagate(state, control, duration, result);
+        }));
+}
+
+ompl::base::ScopedState<> ompl::app::KinematicCarPlanning::getDefaultStartState() const
+{
+    base::ScopedState<base::SE2StateSpace> sSE2(getStateSpace());
+    aiVector3D s = getRobotCenter(0);
+
+    sSE2->setX(s.x);
+    sSE2->setY(s.y);
+    sSE2->setYaw(0.);
+    return sSE2;
+}
+
+void ompl::app::KinematicCarPlanning::setDefaultControlBounds()
+{
+    base::RealVectorBounds cbounds(2);
+    cbounds.low[0] = -5.;
+    cbounds.high[0] = 5.;
+    cbounds.low[1] = -boost::math::constants::pi<double>() * 30. / 180.;
+    cbounds.high[1] = boost::math::constants::pi<double>() * 30. / 180.;
+    getControlSpace()->as<control::RealVectorControlSpace>()->setBounds(cbounds);
+}
+
+void ompl::app::KinematicCarPlanning::ode(const control::ODESolver::StateType& q, const control::Control *ctrl, control::ODESolver::StateType& qdot)
+{
+    const double *u = ctrl->as<control::RealVectorControlSpace::ControlType>()->values;
+
+    // zero out qdot
+    qdot.resize (q.size (), 0);
+
+    qdot[0] = u[0] * cos(q[2]);
+    qdot[1] = u[0] * sin(q[2]);
+    qdot[2] = u[0] * lengthInv_ * tan(u[1]);
+}
+
+void ompl::app::KinematicCarPlanning::postPropagate(const base::State* /*state*/, const control::Control* /*control*/, const double /*duration*/, base::State* result)
+{
+    // Normalize orientation value between 0 and 2*pi
+    const base::SO2StateSpace* SO2 = getStateSpace()->as<base::SE2StateSpace>()
+        ->as<base::SO2StateSpace>(1);
+    auto* so2 = result->as<base::SE2StateSpace::StateType>()
+        ->as<base::SO2StateSpace::StateType>(1);
+    SO2->enforceBounds(so2);
+}
