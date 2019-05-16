e61e8322bcf7e34acb38507b206710c20f80e46e
Olivier Chapeau <ochapeau@protonmail.com>
2019-05-16 22:14:16 +0200

    Added the benchmark in plugin1.py and benchmark results folder

--- ompl_app_hack/gui/plugin1.py
+++ ompl_app_hack/gui/plugin1.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+import argparse
+import os
 import numpy as np
-
 from ompl import base as ob
 from ompl import control as oc
+from ompl import tools as ot
 from ompl import app
 
 #this method is called by ompl_app_custom.py
@@ -20,10 +22,8 @@ def configure(ss):
     cbounds.setHigh(1, 3.0)
     cspace.setBounds(cbounds)
 
-    information = ss.getSpaceInformation()
-
-    information.setStatePropagator(oc.StatePropagatorFn(propagate))
-
+    si = ss.getSpaceInformation()
+    si.setStatePropagator(oc.StatePropagatorFn(propagate))
 
 def propagate(start, control, duration, state):
     """ returns the pose transform for a motion with duration dt of a differential
@@ -47,18 +47,17 @@ def propagate(start, control, duration, state):
     state.setY(y)
     state.setYaw(yaw)
 
-
 def mktr(x, y):
+    """ Returns the translation matrix """
     return np.array([[1, 0, x],
                      [0, 1, y],
                      [0, 0, 1]])
 
-
 def mkrot(theta):
+    """ Returns the rotation matrix """
     return np.array([[np.cos(theta), -np.sin(theta), 0],
                      [np.sin(theta), np.cos(theta), 0],
                      [0, 0, 1]])
-
 
 def ddtr(vl, vr, l, dt):
     """ returns the pose transform for a motion with duration dt of a differential
@@ -70,27 +69,22 @@ def ddtr(vl, vr, l, dt):
     omega = (vr - vl) / (2 * l)  # angular speed of the robot frame
     R = l * (vr + vl) / (vr - vl)
 
-    # Make sure you understand this!
     return mktr(0, R) @ mkrot(omega * dt) @ mktr(0, -R)
 
-
 def homogeneous(x, y, theta):
+
     return np.array([[np.cos(theta), -np.sin(theta), x],
                      [np.sin(theta), np.cos(theta), y],
                      [0, 0, 1]])
 
-
-
-def plan():
-    """this function is used only when running the file as main file\
-    it is not used if used as a plugin"""
+def plan(options):
+    """ Entry point for the planning """
 
     # construct the state space we are planning in
     ss = app.KinematicCarPlanning()
 
-    ss.setEnvironmentMesh('./mesh/UniqueSolutionMaze_env.dae')
-    ss.setRobotMesh('./mesh/car2_planar_robot.dae')
-    # space = ob.SE2StateSpace()
+    ss.setEnvironmentMesh('../../resources/UniqueSolutionMaze_env.dae')
+    ss.setRobotMesh('../../resources/car2_planar_robot.dae')
 
     cspace = ss.getControlSpace()
 
@@ -115,15 +109,22 @@ def plan():
     goal().setY(25)
     goal().setYaw(0.0)
 
-    #si = ss.getSpaceInformation()
-
-    information = ss.getSpaceInformation()
-
-    information.setStatePropagator(oc.StatePropagatorFn(propagate))
+    # space information
+    si = ss.getSpaceInformation()
+    si.setStatePropagator(oc.StatePropagatorFn(propagate))
 
     # set the start and goal states
     ss.setStartAndGoalStates(start, goal, 0.05)
 
+    if not options.bench:
+        planOnce(ss)
+    else:
+        planBenchmark(ss, si)
+
+def planOnce(ss):
+    """ Plan the path once,
+    note: this function is used only when running the file as main file
+    it is not used if used as a plugin"""
 
     # attempt to solve the problem
     solved = ss.solve(20.0)
@@ -131,13 +132,53 @@ def plan():
     if solved:
         # print the path to screen
         print("Found solution:\n%s" % ss.getSolutionPath().printAsMatrix())
+        # save the path to a file
         with open('path.txt', 'w') as outFile:
             outFile.write(ss.getSolutionPath().printAsMatrix())
+
+def planBenchmark(ss, si):
+    # setting the real vector state space bounds
+    se2bounds = ob.RealVectorBounds(2)
+    se2bounds.setLow(0.0)
+    se2bounds.setHigh(10.0)
+    ss.getStateSpace().setBounds(se2bounds)
+
+    # instantiating the benchmark object
+    b = ot.Benchmark(ss, "benchmarking")
+    
+    # b.addExperimentParameter("volume.min.x", "FLOAT", 55.0)
+
+    # adding the planners
+    b.addPlanner(oc.EST(si))
+    b.addPlanner(oc.KPIECE1(si))
+    b.addPlanner(oc.PDST(si))
+    b.addPlanner(oc.RRT(si))
+    b.addPlanner(oc.SST(si))
+    #b.addPlanner(oc.SyclopEST(si))
+    #b.addPlanner(oc.SyclopRRT(si))
+
+    # instantiating the benchmark request
+    req = ot.Benchmark.Request()
+    req.maxTime = 20.0 # time limit allowed for every planner execution (seconds)
+    req.maxMem = 1000.0 # maximum memory allowed for every planner execution (MB)
+    req.runCount = 50 # number of runs for every planner execution
+    req.displayProgress = True
+
+    # running the benchmark
+    b.benchmark(req)
+
+    # saving the result to a file of the form ompl_host_time.log
+    b.saveResultsToFile();
 
 def version():
     return 2
 
 if __name__ == "__main__":
-    plan()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--bench", action="store_true",
+    help="Do benchmarking on provided planner list.")
+
+    plan(parser.parse_args())
 
 print(__file__,'loaded')
